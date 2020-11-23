fb1414d57e081bde56e804be1b30f46517074abe
Nikodem Wrosz <wrosznikodem@gmail.com>
2020-11-23 16:31:49 +0100

    added Lab1 again

--- Lab1/mysite/settings.py
+++ Lab1/mysite/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import os
 import django_heroku
 from pathlib import Path
 
@@ -131,3 +132,5 @@ STATICFILES_DIRS = (
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 django_heroku.settings(locals())
+
+print("base dir path", BASE_DIR)
